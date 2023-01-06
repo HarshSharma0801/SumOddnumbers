8aa9a437a5054fe6acfb5c26160113a5d9f09951
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-01-06 18:11:31 +0530

    Initial commit

---
+++ src/SumOddRange.java
@@ -0,0 +1,31 @@
+public class SumOddRange {
+    public static void main(String[] args) {
+        System.out.println("sum is equal to "+ sumOdd(1,100));
+
+    }
+    public static boolean isOdd(int number){
+        if (number>0){
+            return number % 2 != 0;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static int sumOdd(int start, int end){
+        int sum = 0;
+        if (start>0& end>0 & end>=start){
+        for (int i=start ; i<=end; i++){
+            if (isOdd(i)){
+                 sum+=i;
+
+            }
+        }
+        return sum;
+    }
+        else{
+            return -1;
+        }
+
+    }
+}
